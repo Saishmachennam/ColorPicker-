be2d387ff724d8d5650dcad99ac9c86a6ff05ff3
Saishmachennam <134641196+Saishmachennam@users.noreply.github.com>
2024-04-01 22:50:37 +0530

    Add files via upload

---
+++ ColorPicker.js
@@ -0,0 +1,26 @@
+let bg = document.getElementById("bgCon");
+let colorName = document.getElementById("color");
+
+function button1() {
+    bg.style.backgroundColor = "#e0e0e0";
+    colorName.textContent = "#e0e0e0";
+    colorName.style.color = "#e0e0e0";
+}
+
+function button2() {
+    bg.style.backgroundColor = "#6fcf97";
+    colorName.textContent = "#6fcf97";
+    colorName.style.color = "#6fcf97";
+}
+
+function button3() {
+    bg.style.backgroundColor = "#56ccf2";
+    colorName.textContent = "#56ccf2";
+    colorName.style.color = "#56ccf2";
+}
+
+function button4() {
+    bg.style.backgroundColor = "#bb6bd9";
+    colorName.textContent = "#bb6bd9";
+    colorName.style.color = "#bb6bd9";
+}
